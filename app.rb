7aaa7844f48f3005d195c221ee2233840d2bdf27
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-07-15 16:42:14 -0400

    all routes are working

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get "/name" do
+		"My name is Jay"
+	end
+
+	get "/hometown" do
+		"My hometown is yadayada"
+	end
+
+	get "/favorite-song" do
+		"My favorite song is dksod"
+	end
 end
